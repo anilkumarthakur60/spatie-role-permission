97bd87b04c8f895019af17934d5ad25baaa41d0f
anil kumar thakur <anilkumarthakur60@gmail.com>
2021-06-25 11:37:47 +0545

    commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="col-sm-6 ">
 
-        <h4 class="text-lg-right text-md-right landind_text  my-5 pr-lg-4 text-primary text-uppercase ">Home</h4>
+        <h4 class="text-lg-right text-md-center text-sm-center  landind_text  my-5 pr-lg-4 text-primary text-uppercase ">Home</h4>
         <div class="vertical_center_login_form  w-auto float-lg-right text-sm-center ">
             <div class="">
                 <h1 class="text-lg-right text-primary landind_text">QUALITY</h1>
